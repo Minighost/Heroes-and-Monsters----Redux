07baf38ef836bfc276137fb7a38fb3a700a0589f
Minighost <maesternniw@gmail.com>
2018-02-01 23:27:09 -0800

    farmer update

---
+++ Farmer.java
@@ -0,0 +1,28 @@
+public class Farmer extends obj{
+    private int x, y;
+    private String name;
+    private String item;
+    
+    public Farmer(int x, int y, String n, String i){
+        this.x = x;
+        this.y = y;
+        this.name = n;
+        this.item = i;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String newName){
+        this.name = newName;
+    }
+    
+    public String getItem(){
+        return this.item;
+    }
+    
+    public String toString(){
+        return "Name: " + this.name + ", Item: " + this.item;
+    }
+}
