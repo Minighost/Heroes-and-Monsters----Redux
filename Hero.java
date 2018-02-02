ba9d46d069a3ea0303c68be405a71f639b480948
Minighost <maesternniw@gmail.com>
2018-02-02 13:47:04 -0800

    hero update

--- Hero.java
+++ Hero.java
@@ -4,48 +4,64 @@ public class Hero extends Entity{
     private String name;
     private Weapon weapon;
     private Armor armor;
-    
-    public Hero(int x, int y, int h, int d, Weapon w, Armor a){
+
+    public Hero(int x, int y, Weapon w, Armor a){
         this.x = x;
         this.y = y;
-        this.hp = h;
-        this.dmg = d;
+        this.hp = 100;
+        this.dmg = 10;
         this.weapon = w;
         this.armor = a;
     }
     
+    public int getX(){
+        return this.x;
+    }
+    
+    public void setX(int newX){
+        this.x = newX;
+    }
+    
+    public int getY(){
+        return this.y;
+    }
+    
+    public void setY(int newY){
+        this.y = newY;
+    }
+
     public int getHP(){
         return this.hp;
     }
-    
+
     public void setHP(int newHP){
         this.hp = newHP;
     }
-    
+
     public int getDMG(){
         return this.dmg;
     }
-    
+
     public void setDMG(int newDMG){
         this.dmg = newDMG;
     }
-    
+
     public Weapon getWeapon(){
         return this.weapon;
     }
-    
+
     public void setWeapon(Weapon newWeap){
         this.weapon = newWeap;
     }
-    
+
     public Armor getArmor(){
         return this.armor;
     }
-    
+
     public void setArmor(Armor newArmor){
         this.armor = newArmor;
     }
-    
+
     public String toString(){
         return "Name: " + this.name + ", HP: " + this.hp + ", Weapon: " + this.weapon.toString() + ", Damage: " + this.dmg + ", Armor: " + this.armor.toString();
     }
