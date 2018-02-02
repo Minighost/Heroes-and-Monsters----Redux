9927d157b144b1af9236865b54f26ba963c568de
Minighost <maesternniw@gmail.com>
2018-02-01 23:14:12 -0800

    monster update

---
+++ Monster.java
@@ -0,0 +1,34 @@
+public class Monster extends obj{
+    private int x, y;
+    private int hp;
+    private int dmg;
+    private String name;
+    
+    public Monster(int x, int y, int h, int d, String n){
+        this.x = x;
+        this.y = y;
+        this.hp = h;
+        this.dmg = d;
+        this.name = n;
+    }
+    
+    public int getHP(){
+        return this.hp;
+    }
+    
+    public void setHP(int newHP){
+        this.hp = newHP;
+    }
+    
+    public int getDMG(){
+        return this.dmg;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String newName){
+        this.name = newName;
+    }
+}
