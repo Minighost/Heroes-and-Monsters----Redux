4382cc002a28fbd143012182979c269299939a69
Minighost <maesternniw@gmail.com>
2018-02-01 23:22:59 -0800

    armor update

---
+++ Armor.java
@@ -0,0 +1,24 @@
+public class Armor{
+    private int hp;
+    private int x, y;
+    private String type;
+    
+    public Armor(int x, int y, int hp, String t){
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+        this.type = t;
+    }
+    
+    public int getHP(){
+        return this.hp;
+    }
+    
+    public void setHP(int newHP){
+        this.hp = newHP;
+    }
+    
+    public String toString(){
+        return "Type: " + this.type + "; Integrity: " + this.hp;
+    }
+}
---
+++ Hero.java
@@ -0,0 +1,52 @@
+public class Hero extends obj{
+    private int x, y;
+    private int hp, dmg;
+    private String name;
+    private Weapon weapon;
+    private Armor armor;
+    
+    public Hero(int x, int y, int h, int d, Weapon w, Armor a){
+        this.x = x;
+        this.y = y;
+        this.hp = h;
+        this.dmg = d;
+        this.weapon = w;
+        this.armor = a;
+    }
+    
+    public int getHP(){
+        return this.hp;
+    }
+    
+    public void setHP(int newHP){
+        this.hp = newHP;
+    }
+    
+    public int getDMG(){
+        return this.dmg;
+    }
+    
+    public void setDMG(int newDMG){
+        this.dmg = newDMG;
+    }
+    
+    public Weapon getWeapon(){
+        return this.weapon;
+    }
+    
+    public void setWeapon(Weapon newWeap){
+        this.weapon = newWeap;
+    }
+    
+    public Armor getArmor(){
+        return this.armor;
+    }
+    
+    public void setArmor(Armor newArmor){
+        this.armor = newArmor;
+    }
+    
+    public String toString(){
+        return "Name: " + this.name + ", HP: " + this.hp + ", Weapon: " + this.weapon.toString() + ", Damage: " + this.dmg + ", Armor: " + this.armor.toString();
+    }
+}
