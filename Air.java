ae3025dd8d4178dee7eb4402e757a92a43f36da9
Minighost <maesternniw@gmail.com>
2018-02-05 13:29:13 -0800

    air

---
+++ Air.java
@@ -0,0 +1,20 @@
+public class Air extends Entity{
+    private int x, y;
+    
+    public Air(){
+    }
+    
+    public int getX(){
+        return this.x;
+    }
+    
+    public int getY(){
+        return this.y;
+    }
+    
+    public void setX(){
+    }
+    
+    public void setY(){
+    }
+}
